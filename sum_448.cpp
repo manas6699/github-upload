a7b6b69a174eeed31b97a11f8f325d9e31f422f9
Manas <manasbaroi6699@gmail.com>
2021-02-13 22:01:36 +0530

    "initial commit - moving project"

---
+++ sum_448.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+using namespace std ;
+int main ()
+{
+int a,b;
+cout<<"Enter the 1st number"<<endl;
+cin>>a;
+cout<<"Enter the 2nd number"<<endl;
+cin>>b;
+cout<<"The sum is "<<(a+b);
+
+return 0;
+}
+// "initial commit - moving project"
